7764901889595cc36d8442b048fcd8c99fa1fb2e
Dev2 <Dev2@gmail.com>
2025-05-16 18:16:12 +0530

    Initial Commit

---
+++ DockerSB-RestApp/src/main/java/com/vk/controller/WishController.java
@@ -0,0 +1,18 @@
+package com.vk.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wish")
+public class WishController {
+
+	@GetMapping("/msg/{name}")
+	public ResponseEntity<String> wishMessage(@PathVariable(name="name") String name){
+		return new ResponseEntity<String>("Welcome "+name , HttpStatus.OK);
+	}
+}
